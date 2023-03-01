a3d8272aed209cbd537b60b77e32950a6378e1cb
keraemoe <weekera@gmail.com>
2023-03-02 00:01:22 +0600

    add: Layouts

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+﻿import React from 'react';
+import s from './Footer.module.scss'
+import facebook from '../../assets/facebook.png'
+import twitter from '../../assets/twitter.png'
+import instagram from '../../assets/instagram.png'
+import footer_logo from '../../assets/footer_logo.png'
+
+const Footer = () => {
+    return (
+        <>
+            <div className={s.footer_container}>
+                <div className={s.footer}>
+                    <div className={s.logo}>
+                        <img src={footer_logo} alt="footer-log" />
+                    </div>
+                    <div className={s.society}>
+                        <h1>Наши контакты</h1>
+                        <div className={s.contacts}>
+                            <img src={facebook} alt="facebook-logo" />
+                            <img src={instagram} alt="instagram-logo" />
+                            <img src={twitter} alt="twitter-logo" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+﻿import React from 'react';
+import s from './Header.module.scss'
+import logo from '../../assets/logo.png'
+import busket from '../../assets/busket.png'
+
+const Header = () => {
+    return (
+        <>
+            <div id='container'>
+                <div className={s.header}>
+                    <nav>
+                        <ul>
+                            <li>О нас</li>
+                            <li>Продукция</li>
+                            <li>Полезные статьи</li>
+                            <li>Контакты</li>
+                        </ul>
+                    </nav>
+                    <div className={s.busket}>
+                        <img src={busket} alt="busket" />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Header;
