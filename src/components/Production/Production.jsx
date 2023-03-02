e8f632c342429b8a9494c334fa63d0c44fd40d97
keraemoe <weekera@gmail.com>
2023-03-02 17:56:26 +0600

    add: Production

--- src/components/Production/Production.jsx
+++ src/components/Production/Production.jsx
@@ -1,16 +1,17 @@
 ﻿import s from './Production.module.scss'
 import React from 'react';
-import slide1 from '../../assets/slide1.png'
-import slide2 from '../../assets/slide2.png'
-
+import './Production.css'
+import CardSlide from '../CardSlide/CardSlide';
 
 const Production = () => {
+
     return (
         <>
             <div className={s.production_container}>
                 <div className={s.production}>
                     <h1>Наша продукция</h1>
                     <div className={s.slider}>
+                        <CardSlide />
                     </div>
                 </div>
             </div>
