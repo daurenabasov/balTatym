4b9a1821b0c37caeb6a7f0a71df5937e57aaa063
keraemoe <weekera@gmail.com>
2023-03-04 21:29:02 +0600

    add: Cards Production

---
+++ src/Pages/Home/HomeComponents/Cards/Cards.jsx
@@ -0,0 +1,68 @@
+﻿import React from 'react';
+import s from './Cards.module.scss'
+import slide1 from '../../../../Assets/slide1.png'
+import slide2 from '../../../../Assets/slide2.png'
+import mini from '../../../../Assets/mini_busket.png'
+
+
+const Cards = () => {
+    const cards = [
+        {
+            id: 1,
+            img: slide1,
+            name: 'Сотовый мед',
+            price: '550 сом / л'
+        },
+        {
+            id: 2,
+            img: slide2,
+            name: 'Цветочный мед',
+            price: '900 сом / л'
+        },
+        {
+            id: 3,
+            img: slide2,
+            name: 'Цветочный мед',
+            price: '900 сом / л'
+        },
+        {
+            id: 4,
+            img: slide2,
+            name: 'Цветочный мед',
+            price: '900 сом / л'
+        },
+        {
+            id: 2,
+            img: slide2,
+            name: 'Цветочный мед',
+            price: '900 сом / л'
+        },
+        {
+            id: 2,
+            img: slide2,
+            name: 'Цветочный мед',
+            price: '900 сом / л'
+        }
+    ]
+    return (
+        <>
+            {cards.map((card) => (
+                <div className={s.card_container}>
+                    <img src={card.img} alt="card" />
+                    <div className={s.card}>
+                        <div className={s.info}>
+                            <h1>{card.name}</h1>
+                            <p>{card.price}</p>
+                        </div>
+                        <button>
+                            В корзину
+                            <img src={mini} alt="" />
+                        </button>
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+};
+
+export default Cards;
--- src/Pages/Home/HomeComponents/Production/Production.jsx
+++ src/Pages/Home/HomeComponents/Production/Production.jsx
@@ -1,49 +1,23 @@
 ﻿import s from './Production.module.scss'
-import React from 'react';
+import React, { useState } from 'react';
 import slide1 from '../../../../Assets/slide1.png'
+import Card from '../Cards/Cards';
 
 
 const Production = () => {
-    const bal = [
-        {
-            id: 1,
-            img: slide1,
-            title: "LOX"
-        },
-        {
-            id: 2,
-            img: slide1,
-            title: "LOX"
-        },
-        {
-            id: 3,
-            img: slide1,
-            title: "LOX"
-        }, {
-            id: 3,
-            img: slide1,
-            title: "LOX"
-        },
-        {
-            id: 4,
-            img: slide1,
-            title: "LOX"
-        },
-        {
-            id: 5,
-            img: slide1,
-            title: "LOX"
-        }
-    ]
     return (
         <>
             <div className={s.production_container}>
                 <div className={s.production}>
-                    <h1>Наша продукция</h1>
+                    <div className={s.tag}>
+                        <h1>Каталог</h1>
+                        <button>Подробнее</button>
+                    </div>
                     <div className={s.catalog}>
-
+                        <Card />
                     </div>
                 </div>
+                <button>fdf</button>
             </div>
         </>
     );
